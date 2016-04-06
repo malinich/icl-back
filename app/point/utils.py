17dd1aa23539d60e2c7d8962a5cd3cbb9e5a6f53
malinich <miltrix@yandex.ru>
2016-04-06 17:02:36 +0300

    change iterator for save points

--- app/point/utils.py
+++ app/point/utils.py
@@ -1,13 +1,25 @@
 import xlrd
 import functools
+from itertools import chain
+
+
+def get_rows(sheet):
+    yield from sheet.get_rows()
+
+
+def get_sheets(work):
+    yield from work.sheets()
 
 
 def save_points(file):
     work_book = xlrd.open_workbook(file)
 
-    for sheet in work_book.sheets():
-        for row in sheet.get_rows():
-            yield tuple(float(cell.value) for cell in row)
+    sheets = get_sheets(work_book)
+    rows = (get_rows(sheet) for sheet in sheets)
+    row = chain(*rows)
+
+    for cells in row:
+        yield tuple(float(c.value) for c in cells)
 
 
 def catch_error(*, errors: tuple, rise_up: type(Exception), message: dict):
