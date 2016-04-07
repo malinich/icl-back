410a89733ac07615a3766b8261b5181dc0041a25
malinich <miltrix@yandex.ru>
2016-04-07 12:24:16 +0300

    remove redundant generator function

--- app/point/utils.py
+++ app/point/utils.py
@@ -3,19 +3,11 @@ import functools
 from itertools import chain
 
 
-def get_rows(sheet):
-    yield from sheet.get_rows()
-
-
-def get_sheets(work):
-    yield from work.sheets()
-
-
 def save_points(file):
     work_book = xlrd.open_workbook(file)
 
-    sheets = get_sheets(work_book)
-    rows = (get_rows(sheet) for sheet in sheets)
+    sheets = (sheet for sheet in work_book.sheets())
+    rows = (sheet.get_rows() for sheet in sheets)
     row = chain(*rows)
 
     for cells in row:
